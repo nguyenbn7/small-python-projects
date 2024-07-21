88255a1cdc49a6f50b8b8a545ae6649cc69afc8b
nguyenbn7 <nguyenbn.email@gmail.com>
2024-07-21 18:18:30 +0700

    update extract phone and email

--- phone_and_email_extractor/main.py
+++ phone_and_email_extractor/main.py
@@ -1,6 +1,8 @@
 import re
 
-phone_regex_pattern = re.compile(
+import pyperclip
+
+phone_pattern = re.compile(
     r"""(
         (\d{3}|\(\d{3}\))?                  # area code
         (\s|-|\.)?                          # seperator
@@ -11,3 +13,33 @@ phone_regex_pattern = re.compile(
     )""",
     re.VERBOSE,
 )
+
+email_pattern = re.compile(
+    r"""(
+    [a-zA-Z0-9._%+-]+               # username
+    @                               # @ symbol
+    [a-zA-Z0-9.-]+                  # domain name
+    (\.[a-zA-Z]{2,4})               # dot-something
+    )""",
+    re.VERBOSE,
+)
+
+text = str(pyperclip.paste())
+
+matches = []
+
+for groups in phone_pattern.findall(text):
+    phoneNum = "-".join([groups[1], groups[3], groups[5]])
+    if groups[8] != "":
+        phoneNum += " x" + groups[8]
+    matches.append(phoneNum)
+
+for groups in email_pattern.findall(text):
+    matches.append(groups[0])
+
+if len(matches) > 0:
+    pyperclip.copy("\n".join(matches))
+    print("Copied to clipboard:")
+    print("\n".join(matches))
+else:
+    print("No phone numbers or email addresses found.")
