ecaff4d2303448db44a9c4a0363a060d7632ffb9
nguyenbn7 <nguyenbn.email@gmail.com>
2024-07-23 13:21:07 +0700

    generate capitals quiz application

--- random_quiz_generator/__main__.py
+++ random_quiz_generator/__main__.py
@@ -1,4 +1,5 @@
 import random
+from os import path, makedirs
 
 capitals = {
     "Alabama": "Montgomery",
@@ -53,13 +54,44 @@ capitals = {
     "Wyoming": "Cheyenne",
 }
 
+BASE_DIR = path.dirname(path.abspath(__file__))
+quizzes_dir = path.join(BASE_DIR, "quizzes")
+makedirs(quizzes_dir, exist_ok=True)
+quiz_answers_dir = path.join(BASE_DIR, "quiz_answers")
+makedirs(quiz_answers_dir, exist_ok=True)
 
-for quizNum in range(35):
-    # TODO: Create the quiz and answer key files.
+for quiz_num in range(35):
+    with open(
+        path.join(quizzes_dir, f"capitalsquiz{quiz_num + 1}.txt"), "w"
+    ) as quiz_file:
+        with open(
+            path.join(quiz_answers_dir, f"capitalsquiz_answers{quiz_num + 1}.txt"), "w"
+        ) as answer_key_file:
+            # Write out the header for the quiz.
+            quiz_file.write("Name:\n\nDate:\n\nPeriod:\n\n")
+            quiz_file.write((" " * 20) + f"State Capitals Quiz (Form {quiz_num + 1})")
+            quiz_file.write("\n\n")
 
-    # TODO: Write out the header for the quiz
+            # Shuffle the order of the states.
+            states = list(capitals.keys())
+            random.shuffle(states)
 
-    # TODO: Shuffle the order of the states.
+            for question_num in range(len(capitals)):
+                correct_answer = capitals[states[question_num]]
+                wrong_answers = list(capitals.values())
 
-    # TODO: Loop through all 50 states, making a question for each
-    pass
+                del wrong_answers[wrong_answers.index(correct_answer)]
+                wrong_answers = random.sample(wrong_answers, 3)
+                answer_options = wrong_answers + [correct_answer]
+                random.shuffle(answer_options)
+
+                quiz_file.write(
+                    f"{question_num + 1}. What is the capital of {states[question_num]}?\n"
+                )
+                for i, answer in enumerate(answer_options):
+                    quiz_file.write(f"    {'ABCD'[i]}. {answer}\n")
+                quiz_file.write("\n")
+
+                answer_key_file.write(
+                    f"{question_num + 1}. {'ABCD'[answer_options.index(correct_answer)]}\n"
+                )
