9cfc1fa807d689138c1cdfbb4c6454ae21179f89
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 15:56:49 +0700

    update extract phone and email

--- phone_and_email_extractor/test.py
+++ phone_and_email_extractor/test.py
@@ -1,14 +1,15 @@
 import re
 import unittest
-from phone_and_email_extractor.main import phone_regex_pattern
+
+from phone_and_email_extractor.main import phone_pattern
 
 
 class TestPhoneRegex(unittest.TestCase):
 
-    def test_(self):
+    def test_1(self):
         given = "(212)-456-7890"
 
-        match_obj = re.match(phone_regex_pattern, given)
+        match_obj = phone_pattern.match(given)
 
         self.assertIsNotNone(match_obj)
         self.assertEqual(match_obj.group(2), "(212)")
