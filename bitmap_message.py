0440b21aba25a0d544480963a5d8744c486bbc8f
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-05-13 12:44:33 +0700

    refactor bitmap message prog

--- bitmap_message.py
+++ bitmap_message.py
@@ -1,3 +1,9 @@
+"""Bitmap Message
+Displays a text message according to the provided bitmap image.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, artistic
+"""
+
 import sys
 
 bitmap = """
@@ -24,17 +30,28 @@ bitmap = """
 ....................................................................
 """
 
-print("Bitmap Message, by Al Sweigart al@inventwithpython.com")
-print("Enter the message to display with the bitmap.")
-message = input("> ")
 
-if message == "":
-    sys.exit()
+def main():
+    print("Bitmap Message")
+    print("Enter the message to display with the bitmap.")
+    message = input("> ")
 
-for line in bitmap.splitlines():
-    for i, bit in enumerate(line):
-        if bit == " ":
-            print(" ", end="")
-        else:
-            print(message[i % len(message)], end="")
+    if message == "":
+        sys.exit()
+
+    for line in bitmap.splitlines():
+        for i, bit in enumerate(line):
+            if bit == " ":
+                print(" ", end="")
+            else:
+                print(message[i % len(message)], end="")
+        print()
+
     print()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(130)
